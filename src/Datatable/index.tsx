c3d23ad6d230a875f93822e24f6c057fdb77867c
Mehradi <mehradi.info@gmail.com>
2023-11-03 14:36:48 +0100

    Adding UI/UX CRUD for contacts

--- src/Datatable/index.tsx
+++ src/Datatable/index.tsx
@@ -1,18 +1,59 @@
 import React, { FC } from "react";
-import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
+import {
+  DataGrid,
+  GridRowsProp,
+  GridColDef,
+  GridValueGetterParams,
+  GridActionsCellItem,
+  GridRowId,
+} from "@mui/x-data-grid";
 import { Contact, IResult } from "../model/contact.model";
-
-const columns: GridColDef[] = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "name", headerName: "Name", width: 130 },
-  { field: "email", headerName: "Email", width: 130 },
-  { field: "contact", headerName: "Contact", width: 130 },
-];
+import {
+  Edit as EditIcon,
+  DeleteOutline as DeleteIcon,
+} from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
 
 const DataTable: FC<IResult<Contact[]>> = ({ data }) => {
+  const navigate = useNavigate();
+  const handleEditClick = (id: GridRowId) => () => {
+    navigate(`/contact/${id}`);
+  };
+  const handleDeleteClick = (id: GridRowId) => () => {};
+  const columns: GridColDef[] = [
+    { field: "id", headerName: "ID", width: 70 },
+    { field: "name", headerName: "Name", width: 130 },
+    { field: "email", headerName: "Email", width: 130 },
+    { field: "contact", headerName: "Contact", width: 130 },
+    {
+      field: "actions",
+      type: "actions",
+      headerName: "Actions",
+      width: 100,
+      cellClassName: "actions",
+      getActions: ({ id }) => {
+        return [
+          <GridActionsCellItem
+            icon={<EditIcon />}
+            label="Edit"
+            className="textPrimary"
+            onClick={handleEditClick(id)}
+            color="inherit"
+          />,
+          <GridActionsCellItem
+            icon={<DeleteIcon />}
+            label="Delete"
+            onClick={handleDeleteClick(id)}
+            color="inherit"
+          />,
+        ];
+      },
+    },
+  ];
+
   return (
     <div>
-      <DataGrid rows={data} columns={columns} checkboxSelection />
+      <DataGrid rows={data} columns={columns} />
     </div>
   );
 };
--- src/contact/contact.tsx
+++ src/contact/contact.tsx
@@ -1,6 +1,16 @@
 import React, { FC } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
-const contact: FC = () => {
-  return <div>contact</div>;
+const Contact: FC = () => {
+  const navigate = useNavigate();
+  const params = useParams();
+  const id = params["id"];
+  return (
+    <div>
+      <Link to="/contacts">Back</Link>
+      <br />
+      Contact {id}
+    </div>
+  );
 };
-export default contact;
+export default Contact;
