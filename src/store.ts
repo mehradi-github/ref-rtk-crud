00e79c978e613108c6771672a04a2b53ac39ec3d
Mehradi <mehradi.info@gmail.com>
2023-11-07 13:27:26 +0100

    Showing notification in layout

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { contactsApi } from "./services/contactsApi";
-import snakbarReducer from "./snakbar/snakbarSlice";
 export const store = configureStore({
   reducer: {
     [contactsApi.reducerPath]: contactsApi.reducer,
-    snakbars: snakbarReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(contactsApi.middleware),
